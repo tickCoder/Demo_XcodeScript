bc3e58e0ed85a2429e8dee32e04696b37e52c2bd
tickCoder <tickCoder@users.noreply.github.com>
2016-03-16 13:05:16 +0800

    ＃添加工程

---
+++ Demo_XcodeScript/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_XcodeScript
+//
+//  Created by Claris on 2016.03.16.Wednesday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_XcodeScript/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo_XcodeScript
+//
+//  Created by Claris on 2016.03.16.Wednesday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
